f176fef03b3cdfa70e5a765efdca3a1b104d1b60
decoster <timothee.decoster@elosi.com>
2018-08-29 16:00:33 +0200

    IFrame border

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -3,15 +3,20 @@
         <h1>{{welcome}}</h1>
         <p>Qu'est ce qui court et qui se jette ?</p>
         <div class="iFrame">
-          <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com
-                %2Faei.lille1&tabs=timeline&width=320&height=500&small_header=false&adapt_cont
-                ainer_width=true&hide_cover=false&show_facepile=false&appId"
-            width="320" height="500" style="border:none;overflow:hidden" scrolling="no"
-            frameborder="0" allowTransparency="true" allow="encrypted-media">
-          </iframe>
-          <a class="twitter-timeline" data-lang="fr" data-width="320" data-height="500"
-            data-theme="dark" href="https://twitter.com/AEILille1?ref_src=twsrc%5Etfw"></a>
+          <div class="iframe-border">
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com
+                  %2Faei.lille1&tabs=timeline&width=320&height=500&small_header=false&adapt_cont
+                  ainer_width=true&hide_cover=false&show_facepile=false&appId"
+              width="320" height="500" style="border:none;overflow:hidden" scrolling="no"
+              frameborder="0" allowTransparency="true" allow="encrypted-media">
+            </iframe>
+          </div>
+          <div class="iframe-border">
+            <a class="twitter-timeline" data-width="320" data-height="500"
+              data-lang="fr" data-theme="dark" href="https://twitter.com/AEILille1?ref_src=twsrc%5Etfw">
+            </a>
+          </div>
         </div>
         {{screenWidth}}
         {{screenHeight}}
@@ -47,5 +52,12 @@ export default {
 }
 iframe {
   max-width: 100%;
+}
+.iframe-border {
+  width: 320px;
+  height: 500px;
+  border-style: inset;
+  border-color: #ae2573;
+  border-width: 0.5rem;
 }
 </style>
