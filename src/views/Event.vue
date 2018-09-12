049d45ac58cc0a493e792415bc43002e9c5cee42
decoster <Timothee.decoster@etudiant.univ-lille1.fr>
2018-09-12 18:07:46 +0200

    Add event/photo page

---
+++ src/views/Event.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="event">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Photo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="photo">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Photo',
+};
+</script>
+
+<style scoped>
+</style>
